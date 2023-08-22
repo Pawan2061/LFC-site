2766d6a9e0654e3c3d281d3b9ccbee23f0741a50
pawan2061 <pandeypawan2061@gmail.com>
2023-08-22 19:40:16 +0545

    some changes

--- PortPawan/src/App.jsx
+++ PortPawan/src/App.jsx
@@ -1,33 +1,46 @@
-import FirstFooter from "./Components/FirstFooter";
-import Fixtures from "./Components/Fixtures";
-import Hero from "./Components/Hero";
-import Jurgen from "./Components/Jurgen";
-import LastFooter from "./Components/LastFooter";
-import LowHero from "./Components/LowHero";
-import LowNav from "./Components/LowNav";
-import NewKits from "./Components/NewKits";
-import TopNav from "./Components/TopNav";
-import Vandjik from "./Components/Vandjik";
-import Video from "./Components/Video";
-import ViewGallery from "./Components/ViewGallery";
-import YnwaDesc from "./Components/YnwaDesc";
+// import FirstFooter from "./Components/FirstFooter";
+// import Fixtures from "./Components/Fixtures";
+// import Hero from "./Components/Hero";
+// import Jurgen from "./Components/Jurgen";
+// import LastFooter from "./Components/LastFooter";
+// import LowHero from "./Components/LowHero";
+// import LowNav from "./Components/LowNav";
+// import NewKits from "./Components/NewKits";
+// import TopNav from "./Components/TopNav";
+// import Vandjik from "./Components/Vandjik";
+// import Video from "./Components/Video";
+// import ViewGallery from "./Components/ViewGallery";
+// import YnwaDesc from "./Components/YnwaDesc";
+
+// function App() {
+//   return (
+//     <div className="overflow-hidden">
+//       <TopNav />
+//       <LowNav />
+//       <Hero />
+//       <NewKits />
+//       <LowHero />
+//       <Video />
+//       <ViewGallery />
+//       <Vandjik />
+//       <Fixtures />
+//       <Jurgen />
+//       <YnwaDesc />
+//       <FirstFooter />
+//       <LastFooter />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import Signup from "./RoutingComponents/Signup";
 
 function App() {
   return (
-    <div className="overflow-hidden">
-      <TopNav />
-      <LowNav />
-      <Hero />
-      <NewKits />
-      <LowHero />
-      <Video />
-      <ViewGallery />
-      <Vandjik />
-      <Fixtures />
-      <Jurgen />
-      <YnwaDesc />
-      <FirstFooter />
-      <LastFooter />
+    <div>
+      <Signup />
     </div>
   );
 }
---
+++ PortPawan/src/RoutingComponents/Signup.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div className="bg-[#DC0714] pt-20 pl-10 pr-10 h-screen">
+      <div className="bg-gray-200 flex flex-col justify-center items-center py-20  ">
+        <div className="text-3xl text-red-600 font-semibold py-10">
+          <p>WHY JOIN LFC COMMUNITY?</p>
+        </div>
+        <div className="py-2">
+          Unlock new exicting benefits of joining the million of lfc community.
+          <p className="ml-20 ">JOIN MyLfc ,your free official account.</p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
