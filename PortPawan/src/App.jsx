e642b7b067b3b97376b182f94d88331215e39b3a
pawan2061 <pandeypawan2061@gmail.com>
2023-08-29 19:10:20 +0530

    I added routers in Signup and login buttons in order to direct them to signup and login page.

--- PortPawan/src/App.jsx
+++ PortPawan/src/App.jsx
@@ -1,47 +1,47 @@
-// import FirstFooter from "./Components/FirstFooter";
-// import Fixtures from "./Components/Fixtures";
-// import Hero from "./Components/Hero";
-// import Jurgen from "./Components/Jurgen";
-// import LastFooter from "./Components/LastFooter";
-// import LowHero from "./Components/LowHero";
-// import LowNav from "./Components/LowNav";
-// import NewKits from "./Components/NewKits";
-// import TopNav from "./Components/TopNav";
-// import Vandjik from "./Components/Vandjik";
-// import Video from "./Components/Video";
-// import ViewGallery from "./Components/ViewGallery";
-// import YnwaDesc from "./Components/YnwaDesc";
-
-// function App() {
-//   return (
-//     <div className="overflow-hidden">
-//       <TopNav />
-//       <LowNav />
-//       <Hero />
-//       <NewKits />
-//       <LowHero />
-//       <Video />
-//       <ViewGallery />
-//       <Vandjik />
-//       <Fixtures />
-//       <Jurgen />
-//       <YnwaDesc />
-//       <FirstFooter />
-//       <LastFooter />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import FirstFooter from "./Components/FirstFooter";
+import Fixtures from "./Components/Fixtures";
+import Hero from "./Components/Hero";
+import Jurgen from "./Components/Jurgen";
+import LastFooter from "./Components/LastFooter";
+import LowHero from "./Components/LowHero";
+import LowNav from "./Components/LowNav";
+import NewKits from "./Components/NewKits";
+import TopNav from "./Components/TopNav";
+import Vandjik from "./Components/Vandjik";
+import Video from "./Components/Video";
+import ViewGallery from "./Components/ViewGallery";
+import YnwaDesc from "./Components/YnwaDesc";
 import Login from "./RoutingComponents/Login";
 import Signup from "./RoutingComponents/Signup";
 
 function App() {
   return (
-    <div>
-      <Login />
+    <div className="overflow-hidden">
+      <Routes>
+        <Route path="/sign" element={<Signup />} />
+        <Route
+          index
+          element={
+            <div>
+              <TopNav />
+              <LowNav />
+              <Hero />
+              <NewKits />
+              <LowHero />
+              <Video />
+              <ViewGallery />
+              <Vandjik />
+              <Fixtures />
+              <Jurgen />
+              <YnwaDesc />
+              <FirstFooter />
+              <LastFooter />
+            </div>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- PortPawan/src/Components/LowNav.jsx
+++ PortPawan/src/Components/LowNav.jsx
@@ -1,53 +1,72 @@
 import React from "react";
-// import PersonIcon from "@mui/icons-material/Person";
+import { Route, Router, Routes } from "react-router-dom";
+
+import { Link } from "react-router-dom";
+import FirstFooter from "./FirstFooter";
+import Fixtures from "./Fixtures";
+import Hero from "./Hero";
+import Jurgen from "./Jurgen";
+import LastFooter from "./LastFooter";
+import LowHero from "./LowHero";
+import NewKits from "./NewKits";
+import Vandjik from "./Vandjik";
+import Video from "./Video";
+import ViewGallery from "./ViewGallery";
+import YnwaDesc from "./YnwaDesc";
+import Login from "../RoutingComponents/Login";
+import Signup from "../RoutingComponents/Signup";
+
 function LowNav() {
   return (
-    <div class="bg-[#DC0714]   border-black p-20">
-      <div class="flex items-center justify-between">
-        <div class="cursor-pointer ">
-          <img
-            src="https://www.liverpoolfc.com/liverpoolfc_crest.png"
-            alt="lfc"
-            class="h-[80px]"
-          />
-        </div>
+    <div>
+      <div class="bg-[#DC0714]   border-black p-20">
+        <div class="flex items-center justify-between">
+          <div class="cursor-pointer ">
+            <img
+              src="https://www.liverpoolfc.com/liverpoolfc_crest.png"
+              alt="lfc"
+              class="h-[80px]"
+            />
+          </div>
 
-        <div class="lg:flex md:hidden sm:hidden  ">
-          <ul class="flex gap-8 items-center list-none text-white font-bold text-2xl">
-            <li className="transition ease-in-out   hover:scale-110   duration-300 ">
-              <button>News&Fixture</button>
-            </li>
-            <li className="transition ease-in-out   hover:scale-110   duration-300 ">
-              <button>Teams</button>
-            </li>
-            <li className="transition ease-in-out   hover:scale-110   duration-300 ">
-              <button>Tickets</button>
-            </li>
-            <li className="transition ease-in-out   hover:scale-110   duration-300 ">
-              <button>Shopping</button>
-            </li>
-            <li className="transition ease-in-out   hover:scale-110   duration-300 ">
-              <button>Shop</button>
-            </li>
-            <li className="transition ease-in-out   hover:scale-110   duration-300 ">
-              <button>Video</button>
-            </li>
-            <li class="ml-2">|</li>
-            <li>
-              <button>Join</button>
-            </li>
-            <li>
-              <button>Login</button>
-            </li>
-            <li className="h-[80px] w-[80px] mt-8">
-              <a href="">
-                <img
-                  src="https://av.sc.com/corp-en/content/images/Lockup-MonoBlack.png"
-                  alt=""
-                />
-              </a>
-            </li>
-          </ul>
+          <div class="lg:flex md:hidden sm:hidden  ">
+            <ul class="flex gap-8 items-center list-none text-white font-bold text-2xl">
+              <li className="transition ease-in-out   hover:scale-110   duration-300 ">
+                <button>News&Fixture</button>
+              </li>
+              <li className="transition ease-in-out   hover:scale-110   duration-300 ">
+                <button>Teams</button>
+              </li>
+              <li className="transition ease-in-out   hover:scale-110   duration-300 ">
+                <button>Tickets</button>
+              </li>
+              <li className="transition ease-in-out   hover:scale-110   duration-300 ">
+                <button>Shopping</button>
+              </li>
+              <li className="transition ease-in-out   hover:scale-110   duration-300 ">
+                <button>Shop</button>
+              </li>
+              <li className="transition ease-in-out   hover:scale-110   duration-300 ">
+                <button>Video</button>
+              </li>
+              <li class="ml-2">|</li>
+              <li>
+                <Link to="/sign">Signup</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+
+              <li className="h-[80px] w-[80px] mt-8">
+                <a href="#">
+                  <img
+                    src="https://av.sc.com/corp-en/content/images/Lockup-MonoBlack.png"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
